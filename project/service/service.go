47dcf92e9740a9627003c9848a0f0b5597b8e6c7
Daniel Mesquita <danielmesquitta123@gmail.com>
2026-02-05 06:58:52 -0300

    refactor: integrate worker for asynchronous ticket processing and update exercise ID

--- project/service/service.go
+++ project/service/service.go
@@ -8,24 +8,31 @@ import (
 	"github.com/labstack/echo/v4"
 
 	ticketsHttp "tickets/http"
+	"tickets/worker"
 )
 
 type Service struct {
 	echoRouter *echo.Echo
+	worker     *worker.Worker
 }
 
 func New(
 	spreadsheetsAPI ticketsHttp.SpreadsheetsAPI,
 	receiptsService ticketsHttp.ReceiptsService,
 ) Service {
-	echoRouter := ticketsHttp.NewHttpRouter(spreadsheetsAPI, receiptsService)
+	w := worker.NewWorker(spreadsheetsAPI, receiptsService)
+
+	echoRouter := ticketsHttp.NewHttpRouter(w)
 
 	return Service{
 		echoRouter: echoRouter,
+		worker:     w,
 	}
 }
 
 func (s Service) Run(ctx context.Context) error {
+	go s.worker.Run(ctx)
+
 	err := s.echoRouter.Start(":8080")
 	if err != nil && !errors.Is(err, stdHTTP.ErrServerClosed) {
 		return err
