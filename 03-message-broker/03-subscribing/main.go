596ad30f2d6f10c1714fed52498e53f02d6e85fb
Daniel Mesquita <danielmesquitta123@gmail.com>
2026-02-05 10:02:25 -0300

    feat: implement NACK handling in message broker and update exercise configuration

--- 03-message-broker/03-subscribing/main.go
+++ 03-message-broker/03-subscribing/main.go
@@ -1,5 +1,33 @@
 package main
 
-func main() {
+import (
+	"context"
+	"fmt"
+	"os"
 
+	"github.com/ThreeDotsLabs/watermill-redisstream/pkg/redisstream"
+	"github.com/redis/go-redis/v9"
+)
+
+func main() {
+	rdb := redis.NewClient(&redis.Options{
+		Addr: os.Getenv("REDIS_ADDR"),
+	})
+
+	subscriber, err := redisstream.NewSubscriber(redisstream.SubscriberConfig{
+		Client: rdb,
+	}, nil)
+	if err != nil {
+		panic(err)
+	}
+
+	msgs, err := subscriber.Subscribe(context.Background(), "progress")
+	if err != nil {
+		panic(err)
+	}
+
+	for msg := range msgs {
+		fmt.Printf("Message ID: %s - %s\n", msg.UUID, string(msg.Payload))
+		msg.Ack()
+	}
 }
