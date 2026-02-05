08a4444e916b3c4af4677c82c04c0bd681847c02
Daniel Mesquita <danielmesquitta123@gmail.com>
2026-02-05 09:55:38 -0300

    chore: add subscribing module

--- 03-message-broker/02-publishing-messages/main.go
+++ 03-message-broker/02-publishing-messages/main.go
@@ -1,5 +1,35 @@
 package main
 
-func main() {
+import (
+	"os"
+	"strconv"
 
+	"github.com/ThreeDotsLabs/watermill"
+	"github.com/ThreeDotsLabs/watermill-redisstream/pkg/redisstream"
+	"github.com/ThreeDotsLabs/watermill/message"
+	"github.com/google/uuid"
+	"github.com/redis/go-redis/v9"
+)
+
+func main() {
+	logger := watermill.NewSlogLogger(nil)
+
+	rdb := redis.NewClient(&redis.Options{
+		Addr: os.Getenv("REDIS_ADDR"),
+	})
+
+	publisher, err := redisstream.NewPublisher(redisstream.PublisherConfig{
+		Client: rdb,
+	}, logger)
+	if err != nil {
+		panic(err)
+	}
+
+	for _, progress := range []int{50, 100} {
+		msg := message.NewMessage(uuid.NewString(), []byte(strconv.Itoa(progress)))
+		err = publisher.Publish("progress", msg)
+		if err != nil {
+			panic(err)
+		}
+	}
 }
