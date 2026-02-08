233e7b15544941af80e961ca8085a8396e32445e
Daniel Mesquita <danielmesquitta123@gmail.com>
2026-02-08 18:42:03 -0300

    feat: update exercise ID, restructure directory for project, and implement event processor with Redis stream subscriber

--- 09-cqrs-events/06-cqrs-with-consumer-groups/main.go
+++ 09-cqrs-events/06-cqrs-with-consumer-groups/main.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"github.com/ThreeDotsLabs/watermill"
+	"github.com/ThreeDotsLabs/watermill-redisstream/pkg/redisstream"
 	"github.com/ThreeDotsLabs/watermill/components/cqrs"
 	"github.com/ThreeDotsLabs/watermill/message"
 	"github.com/redis/go-redis/v9"
@@ -13,5 +14,29 @@ func NewEventProcessor(
 	marshaler cqrs.CommandEventMarshaler,
 	logger watermill.LoggerAdapter,
 ) (*cqrs.EventProcessor, error) {
-	panic("implement me!")
+	return cqrs.NewEventProcessorWithConfig(
+		router,
+		cqrs.EventProcessorConfig{
+			SubscriberConstructor:  newSubscriberConstructor(rdb, logger),
+			Marshaler:              marshaler,
+			GenerateSubscribeTopic: generateSubscribeTopic,
+			Logger:                 logger,
+		},
+	)
+}
+
+func newSubscriberConstructor(
+	rdb *redis.Client,
+	logger watermill.LoggerAdapter,
+) cqrs.EventProcessorSubscriberConstructorFn {
+	return func(params cqrs.EventProcessorSubscriberConstructorParams) (message.Subscriber, error) {
+		return redisstream.NewSubscriber(redisstream.SubscriberConfig{
+			Client:        rdb,
+			ConsumerGroup: "svc-tickets." + params.HandlerName,
+		}, logger)
+	}
+}
+
+func generateSubscribeTopic(params cqrs.EventProcessorGenerateSubscribeTopicParams) (string, error) {
+	return params.EventName, nil
 }
