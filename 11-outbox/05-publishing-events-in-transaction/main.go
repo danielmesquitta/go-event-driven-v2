fbb3a97932d486610ced704b3e2f2ea11a9644e2
Daniel Mesquita <danielmesquitta123@gmail.com>
2026-02-25 08:54:52 -0300

    feat: add new exercise for publishing events in transaction, including main logic and tests, and update module dependencies

---
+++ 11-outbox/05-publishing-events-in-transaction/main.go
@@ -0,0 +1,27 @@
+package main
+
+import (
+	"github.com/ThreeDotsLabs/watermill"
+	watermillSQL "github.com/ThreeDotsLabs/watermill-sql/v4/pkg/sql"
+	"github.com/ThreeDotsLabs/watermill/message"
+	"github.com/jmoiron/sqlx"
+	_ "github.com/lib/pq"
+)
+
+func PublishInTx(
+	message *message.Message,
+	tx *sqlx.Tx,
+) error {
+	publisher, err := watermillSQL.NewPublisher(
+		watermillSQL.TxFromStdSQL(tx.Tx),
+		watermillSQL.PublisherConfig{
+			SchemaAdapter: watermillSQL.DefaultPostgreSQLSchema{},
+		},
+		watermill.NewSlogLogger(nil),
+	)
+	if err != nil {
+		return err
+	}
+
+	return publisher.Publish("ItemAddedToCart", message)
+}
